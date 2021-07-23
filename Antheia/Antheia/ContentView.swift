0a6081ba9ec3aee1b97a41976b7c5cbcc852a0b1
Fanny Li <fannyli704@gmail.com>
2021-07-22 20:42:31 -0400

    Sign Up Test2

--- Antheia/Antheia/ContentView.swift
+++ Antheia/Antheia/ContentView.swift
@@ -30,14 +30,17 @@ struct Login : View {
             ZStack{
                 Circle().foregroundColor(Color(red: 0.92, green: 0.82, blue: 0.72)).scaleEffect(0.8)
                 VStack{
+                    
                     // Sign Up Button
                     Button(action: {}, label: {
                         Text("Sign Up").foregroundColor(.black).font(.title3).fontWeight(.semibold).multilineTextAlignment(.center).padding(.all, 10).padding(.horizontal, 50).background(Color(red: 0.87, green: 0.59, blue: 0.59).opacity(0.8)).cornerRadius(15)
-                    })
+                    }).padding(.bottom, 20)
+                  
                     // Login Button
                     Button(action: {}, label: {
                         Text("Login").foregroundColor(.black).font(.title3).fontWeight(.semibold).multilineTextAlignment(.center).padding(.all, 10).padding(.horizontal, 62).background(Color(red: 0.87, green: 0.59, blue: 0.59).opacity(0.8)).cornerRadius(15)
                     })
+                    
                 }
             }
             VStack{
@@ -45,8 +48,27 @@ struct Login : View {
                 Text("App Name").fontWeight(.heavy).font(.title).padding([.top, .bottom], 100)
             
                 Spacer()
-                
-                
+                // Image of Logo?
+                Image("footprint").resizable().aspectRatio(contentMode: .fit).frame(width: 200, height: 200, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/).padding(.leading, 120).padding(.bottom, 100)
+            
+                HStack{
+                    // Social Media
+                    Spacer()
+                    Button(action: {}, label: {
+                        Image("facebook-logo").resizable().aspectRatio(contentMode: .fit).frame(width: 60, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    })
+                    Spacer()
+                    Button(action: {}, label: {
+                        Image("insta-logo").resizable().aspectRatio(contentMode: .fit).frame(width: 60, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    })
+                    Spacer()
+                    Button(action: {}, label: {
+                        Image("twitter-logo").resizable().aspectRatio(contentMode: .fit).frame(width: 60, height: 60, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    })
+                    Spacer()
+                }.padding(.horizontal, 40)
+          
+
             }
         }
     }
