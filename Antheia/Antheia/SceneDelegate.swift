fa34bb928bb0f016d75bb8846d0c1d5e25c4764e
Erica C <ericachen70819@gmail.com>
2021-08-08 13:45:29 -0400

    calculator view

--- Antheia/Antheia/SceneDelegate.swift
+++ Antheia/Antheia/SceneDelegate.swift
@@ -21,7 +21,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView(viewRouter: ViewRouter())
+       
+        let contentView = CalculatorView()
+//        let contentView = ContentView(viewRouter: ViewRouter())
 //                                        .signUpPage)
 
         // Use a UIHostingController as window root view controller.
