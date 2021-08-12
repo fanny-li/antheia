bc058877b1fe78c56a19cc5322ade2ff7f0b6858
kellyngochoang <kellyhoang0407@gmail.com>
2021-08-11 22:21:13 -0400

    Delete 1 Spacer in Calculator

--- Antheia/Antheia/CalculatorView.swift
+++ Antheia/Antheia/CalculatorView.swift
@@ -62,7 +62,7 @@ struct CalculatorView: View {
     var body: some View {
         
         ScrollView {
-            Spacer()
+            //Spacer()
             Spacer()
             Spacer()
             Spacer()
